2dac7d46d838b086cc343ce0d717ae6fd7b5d6b2
DanielMakokha <danielmakokhan@gmail.com>
2023-08-13 06:03:36 +0300

    mini calc

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-// understanding modules
+const add=require('./calculator/add');
+const div=require('./calculator/div');
+const mul=require('./calculator/mul');
+const sub=require('./calculator/sub');
 
-const {add,sub}=require("./ad");
+// console.log(add(2,3));
 
-console.log(add(2,3));
-console.log(sub(2,3));
-console.log(__dirname);
-console.log(__filename);
+const a=parseInt(process.argv[2]);
+const choice=process.argv[3];
+const b=parseInt(process.argv[4]);
+
+// console.log(a,choice,b);
+if (choice ==='ADD') {
+    console.log(a,choice,b);
+    console.log(add(a,b));
+}else if (choice==='SUB') {
+    console.log(a,choice,b);
+    console.log(sub(a,b));
+}else if(choice ==='DIV'){
+    console.log(a,choice,b);
+    console.log(div(a,b));
+}else if(choice==='MUL'){
+    console.log(a,choice,b);
+    console.log(mul(a,b));
+}else{
+    console.log('Invalid operator,use ADD,SUB DIV OR MUL');
+}
